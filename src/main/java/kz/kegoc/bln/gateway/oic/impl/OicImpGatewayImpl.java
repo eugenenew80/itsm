2b1ddc287fad83977adacea794277c06796f03de
Eugene B <eugenenew80@gmail.com>
2018-03-19 21:19:55 +0600

    -

--- src/main/java/kz/kegoc/bln/gateway/oic/impl/OicImpGatewayImpl.java
+++ src/main/java/kz/kegoc/bln/gateway/oic/impl/OicImpGatewayImpl.java
@@ -4,6 +4,8 @@ package kz.kegoc.bln.gateway.oic.impl;
 import kz.kegoc.bln.gateway.oic.OicConfig;
 import kz.kegoc.bln.gateway.oic.OicImpGateway;
 import kz.kegoc.bln.gateway.oic.TelemetryRaw;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class OicImpGatewayImpl implements OicImpGateway {
+    private static final Logger logger = LoggerFactory.getLogger(OicImpGatewayImpl.class);
     private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
 
     @Override
@@ -33,26 +36,43 @@ public class OicImpGatewayImpl implements OicImpGateway {
 
     @Override
     public List<TelemetryRaw> request(LocalDateTime requestedTime) throws Exception {
+        logger.debug("OicImpGatewayImpl.request started");
+
+        if (requestedTime==null)
+            throw new Exception("requestedTime  must be specified");
+
+        if (points==null || points.isEmpty())
+            throw new Exception("points must be specified");
+
+        if (config==null)
+            throw new Exception("config  must be specified");
+
         String requestedTimeStr = requestedTime.format(timeFormatter);
+        logger.trace("requestedTime: " + requestedTimeStr);
 
         String pointsStr = points.stream()
             .map(t -> t.toString())
             .collect(Collectors.joining(","));
+        logger.trace("points: " + pointsStr);
 
-        List<TelemetryRaw> telemetry = new ArrayList<>();
+        logger.trace("Request data from OIC database started");
+        List<TelemetryRaw> telemetryList = new ArrayList<>();
         try (Connection con = new OicConnectionImpl(config).getConnection()) {
-            try (PreparedStatement pst = con.prepareStatement("exec master..xp_gettidata2 1, '" + requestedTimeStr + "', " + pointsStr)) {
+            String sql = "exec master..xp_gettidata2 1, '" + requestedTimeStr + "', " + pointsStr;
+            try (PreparedStatement pst = con.prepareStatement(sql)) {
                 try (ResultSet rs = pst.executeQuery()) {
                     while (rs.next()) {
                         Long logti = rs.getLong(1);
                         Double val = rs.getDouble(2);
-                        telemetry.add(new TelemetryRaw(logti, val));
+                        telemetryList.add(new TelemetryRaw(logti, val));
                     }
                 }
             }
         }
+        logger.trace("Request data from OIC database completed");
 
-        return telemetry;
+        logger.debug("OicImpGatewayImpl.request completed");
+        return telemetryList;
     }
 
     private OicConfig config;
