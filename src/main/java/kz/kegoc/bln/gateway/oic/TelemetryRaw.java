cac0f0bf8e921c2553838e1ae57d43b644d044a1
eugenenew80 <eugenenew80@gmail.com>
2018-03-19 15:58:05 +0600

    -

--- src/main/java/kz/kegoc/bln/gateway/oic/TelemetryRaw.java
+++ src/main/java/kz/kegoc/bln/gateway/oic/TelemetryRaw.java
@@ -1,8 +1,10 @@
 package kz.kegoc.bln.gateway.oic;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(of= {"logti"})
 public class TelemetryRaw {
     public TelemetryRaw(Long logti, Double val) {
         this.logti = logti;
