b69dd30c4490390ad793fdae6bfb095c8eef921f
eugenenew80 <eugenenew80@gmail.com>
2018-03-26 09:40:35 +0600

    -

--- src/main/java/kz/kegoc/bln/App.java
+++ src/main/java/kz/kegoc/bln/App.java
@@ -5,11 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
