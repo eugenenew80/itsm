0f32102ff336df6c8cdeb3cbb159017cc86a9dea
eugenenew80 <eugenenew80@gmail.com>
2018-06-05 22:22:40 +0600

    -

--- src/main/java/kz/kegoc/bln/gateway/oic/impl/OicDatabaseImpl.java
+++ src/main/java/kz/kegoc/bln/gateway/oic/impl/OicDatabaseImpl.java
@@ -1,6 +1,5 @@
 package kz.kegoc.bln.gateway.oic.impl;
 
-import com.sun.rowset.CachedRowSetImpl;
 import kz.kegoc.bln.gateway.oic.OicConfig;
 import kz.kegoc.bln.gateway.oic.OicDatabase;
 import kz.kegoc.bln.gateway.oic.ServerNumEnum;
@@ -8,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.sql.RowSet;
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
 import java.sql.*;
 
 @RequiredArgsConstructor
@@ -17,7 +18,7 @@ public class OicDatabaseImpl implements OicDatabase {
 
     public RowSet execStatement(String sql) throws Exception {
         logger.debug("Executing SQL command: " + sql);
-        CachedRowSetImpl crs = new CachedRowSetImpl();
+        CachedRowSet crs = RowSetProvider.newFactory().createCachedRowSet();;
         try (Connection con = getConnection(); PreparedStatement pst = con.prepareStatement(sql); ResultSet rs = pst.executeQuery()) {
             crs.populate(rs);
         }
