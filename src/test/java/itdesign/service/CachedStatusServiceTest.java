c565889d3790b884ff66ae98761e187ed91e248d
eugenenew80 <eugenenew80@gmail.com>
2019-11-10 01:33:23 +0600

    Добавил тесты для сервисов кэширования статусов и групп

--- src/test/java/itdesign/service/CachedStatusServiceTest.java
+++ src/test/java/itdesign/service/CachedStatusServiceTest.java
@@ -3,13 +3,11 @@ package itdesign.service;
 import itdesign.App;
 import itdesign.entity.Status;
 import itdesign.repo.StatusRepo;
-import itdesign.service.impl.CashedStatusServiceImpl;
-import org.ehcache.CacheManager;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import static itdesign.helper.EntitiesHelper.*;
 import static org.mockito.Mockito.*;
@@ -17,17 +15,12 @@ import static org.mockito.Mockito.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = App.class, properties = "spring.profiles.active=test")
 public class CachedStatusServiceTest {
+
+    @MockBean
     private StatusRepo mockRepo;
-    private CachedStatusService service;
 
     @Autowired
-    private  CacheManager ehcacheManager;
-
-    @Before
-    public void setUp() {
-        mockRepo = mock(StatusRepo.class);
-        service = new CashedStatusServiceImpl(mockRepo, ehcacheManager);
-    }
+    private CachedStatusService service;
 
     @Test
     public void statusShouldBeCached() {
