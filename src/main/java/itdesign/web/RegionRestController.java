1a055d566d163dc6fe722dc7f1cc422a6fd3f3ef
eugenenew80 <eugenenew80@gmail.com>
2019-11-14 14:51:23 +0600

    Добавление liquibase и скриптов для создания таблиц

--- src/main/java/itdesign/web/RegionRestController.java
+++ src/main/java/itdesign/web/RegionRestController.java
@@ -7,6 +7,7 @@ import itdesign.entity.Region;
 import itdesign.repo.RegionRepo;
 import itdesign.web.dto.LongDto;
 import itdesign.web.dto.RegionDto;
+import itdesign.web.dto.RegionTreeDto;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -15,7 +16,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.dozer.DozerBeanMapper;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
@@ -46,14 +46,76 @@ public class RegionRestController extends BaseController {
 
     @ApiOperation(value="Получить список всех записей")
     @GetMapping(value = "/api/v1/slices/regs", produces = "application/json")
-    public List<RegionDto> getAll() {
+    public List<RegionDto> getAll(@RequestParam(value = "lang", defaultValue = "RU") @ApiParam(value = "Язык", example = "RU") String lang) {
         logger.debug(getClass().getName() + ".getAll()");
 
-        Sort sort = new Sort(Sort.Direction.ASC, "id");
-        return repo.findAll(sort)
+        return repo.findAll()
             .stream()
+            .filter(t -> t.getLang().equals(lang.toUpperCase()))
             .map(transformToDto::apply)
             .collect(Collectors.toList());
+    }
+
+    @ApiOperation(value="Получить список всех записей")
+    @GetMapping(value = "/api/v1/slices/regsTree", produces = "application/json")
+    public RegionTreeDto getTree(@RequestParam(value = "lang", defaultValue = "RU") @ApiParam(value = "Язык", example = "RU") String lang) {
+        logger.debug(getClass().getName() + ".getTree()");
+
+        //Получаем список регионов
+        List<Region> list = repo.findAll()
+            .stream()
+            .filter(t -> t.getLang().equals(lang.toUpperCase()))
+            .collect(Collectors.toList());
+
+        if (list.size() == 0)
+            return null;
+
+        //Корневой элемент
+        Region rootEntity = list.stream()
+            .filter(t -> t.getRegType().equals("00"))
+            .findFirst()
+            .orElseGet(null);
+
+        if (rootEntity == null)
+            return null;
+
+        RegionTreeDto rootDto = new RegionTreeDto();
+        rootDto.setCode(rootEntity.getCode());
+        rootDto.setName(rootEntity.getName());
+
+        //Уровень 1
+        List<Region> childrenLev1 = list.stream()
+            .filter(t -> t.getRegType().equals("01") && t.getCode().substring(0, 2).equals(rootEntity.getCode()))
+            .collect(Collectors.toList());
+
+        List<RegionTreeDto> childrenDtoLev1 = new ArrayList<>();
+        for (Region r : childrenLev1) {
+            RegionTreeDto rd = new RegionTreeDto();
+            rd.setCode(r.getCode());
+            rd.setName(r.getName());
+            childrenDtoLev1.add(rd);
+        }
+        rootDto.setChildren(childrenDtoLev1);
+
+
+        //Уровень 2
+        for (RegionTreeDto rd1 : rootDto.getChildren()) {
+            List<Region> childrenLev2 = list.stream()
+                .filter(t -> t.getRegType().equals("02") && t.getCode().substring(0, 4).equals(rd1.getCode()))
+                .collect(Collectors.toList());
+
+            List<RegionTreeDto> childrenDtoLev2 = new ArrayList<>();
+            for (Region r : childrenLev2) {
+                RegionTreeDto rd2 = new RegionTreeDto();
+                rd2.setCode(r.getCode());
+                rd2.setName(r.getName());
+                rd2.setChildren(new ArrayList<>());
+                childrenDtoLev2.add(rd2);
+            }
+            rd1.setChildren(childrenDtoLev2);
+        }
+
+        return rootDto;
     }
 
 
--- src/main/java/itdesign/web/dto/RegionTreeDto.java
+++ src/main/java/itdesign/web/dto/RegionTreeDto.java
@@ -4,16 +4,18 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-@ApiModel(value = "Region", description = "Регионы")
-@Data
-public class RegionDto {
+import java.util.List;
 
-    @ApiModelProperty(value = "Идентификатор", example = "", position = 0)
-    private Long id;
+@ApiModel(value = "RegionTree", description = "Регионы в виде дерева")
+@Data
+public class RegionTreeDto {
 
     @ApiModelProperty(value = "Код", example = "", position = 1)
     private String code;
 
     @ApiModelProperty(value = "Название", example = "", position = 2)
     private String name;
+
+    @ApiModelProperty(value = "Дочерние регионы", example = "", position = 3)
+    private List<RegionTreeDto> children;
 }
