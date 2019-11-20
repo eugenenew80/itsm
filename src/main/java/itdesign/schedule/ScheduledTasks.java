3e5d6b8df5f7126dfded8908362a24a545786da4
eugenenew80 <eugenenew80@gmail.com>
2019-11-20 16:59:18 +0600

    Добавил формирование отчёта

--- src/main/java/itdesign/schedule/ScheduledTasks.java
+++ src/main/java/itdesign/schedule/ScheduledTasks.java
@@ -1,6 +1,8 @@
 package itdesign.schedule;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,10 +11,13 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ScheduledTasks implements ApplicationListener<ApplicationReadyEvent> {
-    private boolean isReady = false;
+    private static boolean isReady = false;
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    @Scheduled(cron = "30 */5 * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void startImport() {
+        logger.debug("GC started");
+        System.gc();
     }
 
     @Override
